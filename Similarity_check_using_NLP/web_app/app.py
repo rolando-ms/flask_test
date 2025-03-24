4ba7294344e8ea6583208a43fd83c307dee3f76f
rolando-ms <rolando.moralessuarez@gmail.com>
2025-03-24 15:12:37 +0100

    Added Detect similarity resource and helper functions

--- Similarity_check_using_NLP/web_app/app.py
+++ Similarity_check_using_NLP/web_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
+import spacy
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,7 +26,7 @@ class RegisterUser(Resource):
         if user_exist(username):
             return_json = {
                 "status": 301,
-                "message": "Invalid username"
+                "message": "User already registered"
             }
             return jsonify(return_json)
         
@@ -45,8 +46,96 @@ class RegisterUser(Resource):
         }
 
         return jsonify(return_json)
+    
+def verify_password(username, password):
+    if not user_exist(username):
+        return False
+    
+    hashed_password = users.find(
+        {
+            "Username": username
+        }
+    )[0]['Password']
+
+    if bcrypt.hashpw(password.encode('utf8'), hashed_password) == hashed_password:
+        return True
+    return False
+
+
+def count_tokens(username):
+    tokens = users.find(
+        {
+            "Username": username
+        }
+    )[0]["Tokens"]
+    return tokens
+    
+class DetectSimilarity(Resource):
+    def post(self):
+        posted_data = request.get_json()
+
+        username = posted_data["username"]
+        password = posted_data["password"]
+        text1 = posted_data["text1"]
+        text2 = posted_data["text2"]
+
+        if not user_exist(username):
+            return_json = {
+                "status": 301,
+                "message": "Invalid username"
+            }
+            return jsonify(return_json)
+        
+        correct_password = verify_password(username, password)
+
+        if not correct_password:
+            return_json = {
+                "status": 302,
+                "message": "Invalid password"
+            }
+            return jsonify(return_json)
+        
+        tokens = count_tokens(username)
+        
+        if tokens <= 0:
+            return_json = {
+                "status": 303,
+                "message": "Not enough tokens available for this operation "
+                "(%s tokens left)." % (str(tokens))
+            }
+            return jsonify(return_json)
+        
+        nlp = spacy.load('en_core_web_sm')
+
+        text1 = nlp(text1)
+        text2 = nlp(text2)
+
+        ratio = text1.similarity(text2)
+
+        return_json = {
+            "status": 200,
+            "message": "Similarity calculated succesfully.",
+            "similarity": ratio
+        }
+
+        users.update_one(
+            {
+                "Username": username
+            },
+            {
+                "$set":
+                {
+                    "Tokens": tokens - 1
+                }
+            }
+        )
+
+        return jsonify(return_json)
+
+
 
 api.add_resource(RegisterUser, "/registerUser")
+api.add_resource(DetectSimilarity, "/detectSimilarity")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
