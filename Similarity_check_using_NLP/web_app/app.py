96a3ad58334e6c31d0ed718c2bed68848877b675
rolando-ms <rolando.moralessuarez@gmail.com>
2025-03-24 16:13:56 +0100

    Added Admin Document, Admin Resources and User Token check Resource

--- Similarity_check_using_NLP/web_app/app.py
+++ Similarity_check_using_NLP/web_app/app.py
@@ -10,6 +10,7 @@ api = Api(app)
 client = MongoClient("mongodb://db:27017")
 db = client.SimilarityDB
 users = db["Users"]
+admins = db["Admins"]
 
 def user_exist(username):
     if users.count_documents({"Username": username}) == 0:
@@ -131,11 +132,144 @@ class DetectSimilarity(Resource):
         )
 
         return jsonify(return_json)
+    
+def admin_exist(admin):
+    if admins.count_documents({"Admin": admin}) == 0:
+        return False
+    return True
 
+class RegisterAdmin(Resource):
+    def post(self):
+        posted_data = request.get_json()
+
+        admin = posted_data["admin"]
+        password = posted_data["password"]
+
+        if admin_exist(admin):
+            return_json = {
+                "status": 301,
+                "message": "Admin already registered"
+            }
+            return jsonify(return_json)
+        
+        hashed_password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+
+        admins.insert_one(
+            {
+                "Admin": admin,
+                "Password": hashed_password
+            }
+        )
+
+        return_json = {
+            "status": 200,
+            "message": "Admin succesfully added."
+        }
+
+        return jsonify(return_json)
+
+def verify_admin_password(admin, password):
+    if not admin_exist(admin):
+        return False
+    
+    hashed_password = admins.find(
+        {
+            "Admin": admin
+        }
+    )[0]['Password']
+
+    if bcrypt.hashpw(password.encode('utf8'), hashed_password) == hashed_password:
+        return True
+    return False
+    
+class RefillTokens(Resource):
+    def post(self):
+        posted_data = request.get_json()
+
+        admin = posted_data["admin"]
+        password = posted_data["password"]
+        username = posted_data["username"]
+        refill_tokens = posted_data["tokens"]
+
+        if not admin_exist(admin):
+            return_json = {
+                "status": 301,
+                "message": "Invalid admin"
+            }
+            return jsonify(return_json)
+        
+        correct_password = verify_admin_password(admin, password)
+
+        if not correct_password:
+            return_json = {
+                "status": 302,
+                "message": "Invalid admin password"
+            }
+            return jsonify(return_json)
+        
+        tokens = count_tokens(username)
+
+        users.update_one(
+            {
+                "Username": username
+            },
+            {
+                "$set":
+                {
+                    "Tokens": tokens + refill_tokens
+                }
+            }
+        )
+
+        return_json = {
+            "status": 200,
+            "message": "Refill succesful. Tokens available for User: %s = %s tokens" % 
+            (username, str(tokens + refill_tokens))
+        }
+        return jsonify(return_json)
+
+class GetTokens(Resource):
+    def post(self):
+        posted_data = request.get_json()
+
+        username = posted_data["username"]
+        password = posted_data["password"]
+
+        if not user_exist(username):
+            return_json = {
+                "status": 301,
+                "message": "Invalid username"
+            }
+            return jsonify(return_json)
+        
+        correct_password = verify_password(username, password)
+
+        if not correct_password:
+            return_json = {
+                "status": 302,
+                "message": "Invalid password"
+            }
+            return jsonify(return_json)
+        
+        tokens = users.find(
+        {
+            "Username": username
+        }
+        )[0]["Tokens"]
+
+        return_json = {
+            "status": 200,
+            "Number of tokens available": tokens
+        }
+
+        return jsonify(return_json)
 
 
 api.add_resource(RegisterUser, "/registerUser")
 api.add_resource(DetectSimilarity, "/detectSimilarity")
+api.add_resource(RegisterAdmin, "/registerAdmin")
+api.add_resource(RefillTokens, "/refillTokens")
+api.add_resource(GetTokens, "/getTokens")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
